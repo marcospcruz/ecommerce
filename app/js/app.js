542a5a0bcb7b48a06102aa2806fd6aa4aec4adaf
Marcos Pereira da Cruz <marcpcruz@gmail.com>
2016-03-03 16:05:36 -0300

    commit 2

--- app/js/app.js
+++ app/js/app.js
@@ -2,5 +2,21 @@
 
 /* App Module */
 
+var ecommerceApp=angular.module('ecommerceApp',['ngSanitize','ngRoute','ecommerceControllers']);
 
-
+ecommerceApp.config(['$routeProvider',
+	function($routeProvider){
+		$routeProvider.
+			when('/loja',{
+				templateUrl: 	'vitrine.html',
+				controller:	'vitrineController'	
+			}).
+			when('/loja/:idTipoProduto',{
+				templateUrl: 	'vitrine.html',
+				controller:	'vitrineController'	
+			}).
+			otherwise({
+				redirectTo: 	'/loja'
+			});
+	}
+]);
--- app/js/controller.js
+++ app/js/controller.js
@@ -1,15 +1,35 @@
 'use strict';
 
 /* Controllers */
-var ecommerceApp=angular.module('ecommerceApp',[]);
 
-ecommerceApp.controller('ecommerceLayoutCntrl',function($scope,$http){
-	$http.get("http://localhost/xtreme/site2/loja/action/startupAction.php").then(function(response){
-		console.log(JSON.stringify(response));
+var url="http://localhost/xtreme/site2/loja/action";
+
+var ecommerceControllers=angular.module('ecommerceControllers',[]);
+
+ecommerceControllers.controller('ecommerceLayoutCntrl',['$scope','$http',function($scope,$http){
+	$http.get(url+"/startupAction.php").then(function(response){
+
+
+		$scope.totalItensCarrinho=response.data[0].totalItensCarrinho;
+		$scope.myData=response.data[1].categoriasProdutos;		
+		$scope.anoAtual=response.data[2].anoAtual;		
+		//console.log(JSON.stringify($scope.totalItensCarrinho));
+	},function(response){console.log('erro'+JSON.stringify(response));});
+
+	
+}]);
+
+ecommerceControllers.controller('vitrineController',['$scope','$http','$routeParams',function($scope,$http,$routeParams){
+	//$http.get(url+"/vitrineAction.php").then(function(response){
+	$http({
+		method: 'POST',
+		url:	url+"/vitrineAction.php",
+		data:	JSON.stringify({idTipoProduto:$routeParams.idTipoProduto}),
+		headers : { 'Content-Type': 'application/x-www-form-urlencoded' }	
+	}).then(function(response){
 		$scope.myData=response.data[1];
 		$scope.totalItensCarrinho=response.data[0].totalItensCarrinho;
 
 	},function(response){console.log('erro'+JSON.stringify(response));});
+}]);
 
-	
-});
